c6b9799eb6c836c9ffc0819b74a42d4a03b256dc
imsho <92440104+Matthew-HMS@users.noreply.github.com>
2023-10-18 14:52:03 +0800

    Add files via upload

---
+++ course_feedback.py
@@ -0,0 +1,32 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+options = webdriver.ChromeOptions()
+options.add_argument("user-data-dir=C:\\Users\\Matt\\AppData\\Local\\Google\\Chrome\\User Data")
+driver = webdriver.Chrome(options=options)
+driver.get("https://cis.ncu.edu.tw/iNCU/academic/course/courseEvaluate?groupName=wpsNone&loginName=07B707BE05F90152064B011F0240082D064B07BE0721062E&sn=454080071&systemId=57&verifyName=MTEwNDAzNTI1")
+time.sleep(3)
+
+login = driver.find_element(By.XPATH, "//*[@class = 'btn btn-primary']")
+login.click()
+time.sleep(1)
+go_to = driver.find_element(By.XPATH, "//*[@class = 'btn btn-primary']")
+go_to.click()
+time.sleep(3)
+forms = driver.find_elements(By.XPATH, "//*[@class = 'btn btn-default']")
+
+for i in range(len(forms)):
+    forms = driver.find_elements(By.XPATH, "//*[@class = 'btn btn-default']")
+    forms[i].click()
+    time.sleep(1)
+    textarea = driver.find_element(By.XPATH, "//*[@class = 'form-control']")
+    textarea.send_keys("謝謝老師！")
+    time.sleep(1)
+    submit = driver.find_element(By.XPATH, "//*[@class = 'btn btn-light btn-default']")
+    submit.click()
+    time.sleep(1)
+
+
+time.sleep(5)
+driver.quit()
