34632934c0474bb3850814a838d1794ce5548800
Skywalker <imsho0218@gmail.com>
2023-12-04 22:33:17 +0800

    update

--- course_feedback.py
+++ course_feedback.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 options = webdriver.ChromeOptions()
-options.add_argument("user-data-dir=C:\\Users\\{Your name}\\AppData\\Local\\Google\\Chrome\\User Data")
+options.add_argument("user-data-dir=C:\\Users\\Skywalker\\AppData\\Local\\Google\\Chrome\\User Data")
 driver = webdriver.Chrome(options=options)
 driver.get("https://cis.ncu.edu.tw/iNCU/academic/course/courseEvaluate?groupName=wpsNone&loginName=07B707BE05F90152064B011F0240082D064B07BE0721062E&sn=454080071&systemId=57&verifyName=MTEwNDAzNTI1")
 time.sleep(3)
@@ -20,6 +20,12 @@ for i in range(len(forms)):
     forms = driver.find_elements(By.XPATH, "//*[@class = 'btn btn-default']")
     forms[i].click()
     time.sleep(1)
+
+    radios = driver.find_elements(By.XPATH, "//input[@type='radio' and @value='A']")
+    for radio in radios:
+        if not radio.is_selected():
+            radio.click()
+
     textarea = driver.find_element(By.XPATH, "//*[@class = 'form-control']")
     textarea.send_keys("謝謝老師！")
     time.sleep(1)
